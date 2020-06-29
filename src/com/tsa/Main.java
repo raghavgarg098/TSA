77d539e5283bd0f4e91da615335aac9a34d4afda
raghavgarg098 <raghavgarg098@gmail.com>
2020-06-29 12:43:04 +0530

    some changes 4

--- src/com/tsa/Main.java
+++ src/com/tsa/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
   public static void main(String[] args)
   {
+      int j=1;
+      int i=0;
+      while(j==1)
+      {
+          System.out.println("Success ");
+          if(i==10)
+          {
+              break;
+          }
+          i++;
+      }
 
     
   }
